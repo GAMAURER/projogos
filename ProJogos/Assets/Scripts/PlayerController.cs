71a4e94f3e30899aace52e838b1bd858c99e47da
GabrielWetzel <gabriel.rockenbach@acad.pucrs.br>
2020-06-07 12:08:48 -0300

    modificou movimento e add detecção do jogador simples

--- ProJogos/Assets/Scripts/PlayerController.cs
+++ ProJogos/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public bool useForce = false;
+    public bool useForce;
 
-    public float speed = 2f;
-    public float halfsize =0.5f;
+    public float speed;
     private BoxCollider2D boxcol;
-    private Rigidbody2D rigidbody;
+    private Rigidbody2D rb;
     
     // Start is called before the first frame update
     void Start()
     {
-        speed = 10f;
         boxcol = GetComponent<BoxCollider2D>();
-        rigidbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -33,44 +31,8 @@ public class PlayerController : MonoBehaviour
     }
     bool tryMove(Vector2 movement)
     {
-        if(useForce)
-        {
-            rigidbody.AddForce(movement);
+        rb.velocity = movement;
+        return true;
 
-            return true;
-
-
-        }
-        else
-        {
-
-            if (!boxcol.IsTouchingLayers(LayerMask.GetMask("Blocking")))
-
-            {
-                rigidbody.velocity = movement;
-
-                return true;
-            }
-            return false;
-
-        }
-
-
-        /*
-            Vector2 step = movement * Time.deltaTime * speed;
-            int blocklayer = 1 << 8;
-
-            Collider2D hit = Physics2D.OverlapBox((Vector2)transform.position + step, boxcol.size, 0, blocklayer);
-            //RaycastHit2D hitR = Physics2D.Raycast((Vector2)transform.position, movement,step.magnitude, blocklayer);
-            if (hit == null)
-            {
-                transform.Translate(step);
-
-
-                return true;
-            }
-            Debug.Log("hit!=null");
-            return false;
-        */
     }
 }
---
+++ ProJogos/Assets/Scripts/PlayerDetection.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetection : MonoBehaviour
+{
+    public float sightdistance = 5;
+    public Vector2 facing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        facing = new Vector2(-1, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Transform player = GameObject.Find("Player").transform;
+        float dist = Vector2.Distance(transform.position, player.position);
+        if (dist < sightdistance)
+        {
+            if (Vector2.Angle(facing, player.position - transform.position) < 60)
+            {
+
+                GetComponent<BoxCollider2D>().enabled = false;
+                RaycastHit2D hitR = Physics2D.Raycast(transform.position, player.position - transform.position);
+                GetComponent<BoxCollider2D>().enabled = true;
+
+                if (hitR.collider.name == "Player")
+                {
+                    Debug.Log("You were seen");
+                }
+            }
+        }
+
+    }
+}
