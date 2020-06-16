a91ff364b7ba2c9131c707934dca5d37d53bb148
GAMAURER <guilherme.maurer@acad.pucrs.br>
2020-06-15 21:01:02 -0300

    Added Level1

---
+++ ProJogos/Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class enemyMovement : MonoBehaviour
+{
+    [Range(0.1f, 10f)]
+    public float speed;
+    public Vector3 from;
+    private Vector3 target;
+    public Vector3 to;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = from;
+
+    }
+
+
+    void Update()
+    {
+        if (transform.position == from)
+        {
+            target = to;
+        }
+        if (transform.position == to)
+        {
+            target = from;
+        }
+
+        float step = speed * Time.deltaTime;
+        transform.right = target - transform.position;
+        // Move objeto na direção do target
+        transform.position = Vector3.MoveTowards(transform.position,
+            target, step);
+    }
+    // Update is called once per frame
+
+}
