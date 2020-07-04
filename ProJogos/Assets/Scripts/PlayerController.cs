571b77e8ec94f8d8bf1a9de575028917d548a8aa
GabrielWetzel <gabriel.rockenbach@acad.pucrs.br>
2020-07-04 14:13:21 -0300

    adittions

--- ProJogos/Assets/Scripts/PlayerController.cs
+++ ProJogos/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     public float speed;
     private BoxCollider2D boxcol;
     private Rigidbody2D rb;
+    public GameObject dialogue;
+    public int state = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -23,10 +25,18 @@ public class PlayerController : MonoBehaviour
         
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
+        bool interact = Input.GetButton("Fire1");
         Vector2 movement = new Vector2(horizontal, vertical)*speed;
         tryMove(movement);
         
-
+        if(interact)
+        {
+            var canvas = GameObject.Find("Canvas");
+            //Instantiate(dialogue,canvas.transform);
+            var box = canvas.transform.GetChild(0).GetChild(0);
+            box.GetComponent<TextMesh>();
+            
+        }
 
     }
     bool tryMove(Vector2 movement)
