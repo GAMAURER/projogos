49902fffa079ebfbae522a202e7e4cff9d2bf5b9
GabrielWetzel <gabriel.rockenbach@acad.pucrs.br>
2020-07-04 16:16:14 -0300

    getting items and chaning levels

--- ProJogos/Assets/Scripts/Door.cs
+++ ProJogos/Assets/Scripts/Door.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public string levelname = "Level1";
+    public string spawnname = "PlayerSpawn";
     public Vector3 camerapos;
     public Vector3 playerpos;
     void Start()
--- ProJogos/Assets/Scripts/PlayerController.cs
+++ ProJogos/Assets/Scripts/PlayerController.cs
@@ -6,25 +6,37 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     public bool useForce;
+    public bool respawn = false;
+    private string nextSpawn;
+
+    private string currScene;
 
     public float speed;
     private BoxCollider2D boxcol;
     private Rigidbody2D rb;
     public GameObject dialogue;
     public int state = 0;
-    
+    HashSet<string> itemsobtained;
+
     // Start is called before the first frame update
     void Start()
     {
         SceneManager.LoadScene("Level1", LoadSceneMode.Additive);
-        
+        currScene = "Level1";
+
         boxcol = GetComponent<BoxCollider2D>();
         rb = GetComponent<Rigidbody2D>();
+        itemsobtained = new HashSet<string>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (respawn)
+        {
+            transform.position = GameObject.Find(nextSpawn).transform.position;
+            respawn = false;
+        }
         
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
@@ -34,11 +46,37 @@ public class PlayerController : MonoBehaviour
         
         if(interact)
         {
+            /*
             var canvas = GameObject.Find("Canvas");
             //Instantiate(dialogue,canvas.transform);
             var box = canvas.transform.GetChild(0).GetChild(0);
             box.GetComponent<TextMesh>();
-            
+            */
+            var hit = Physics2D.OverlapCircle(transform.position, 1.5f,LayerMask.GetMask("Items"));
+            if (hit != null)
+            {
+                var a = hit.gameObject;
+                itemsobtained.Add(a.name);
+                Debug.Log("Got " + a.name);
+                a.SetActive(false);
+            }
+            hit = null;
+            hit = Physics2D.OverlapCircle(transform.position, 1.5f, LayerMask.GetMask("Doors"));
+            if (hit != null)
+            {
+                var door = hit.gameObject.GetComponent<Door>();
+                SceneManager.UnloadSceneAsync(currScene);
+                var newscene = door.levelname;
+                nextSpawn = door.spawnname;
+                respawn = true;
+                SceneManager.LoadScene(newscene, LoadSceneMode.Additive);
+                currScene = newscene;
+
+                
+               
+                Debug.Log("loadedscene " + newscene);
+            }
+
         }
 
     }
